00e85a0a3d3c69b5f82534b297f39f2ebff114b5
RKM2022 <100585944+RKM2022@users.noreply.github.com>
2022-03-15 17:34:16 +0000

    Update Transformation_script_new.R

--- R/Transformation_script_new.R
+++ R/Transformation_script_new.R
@@ -1,22 +1,10 @@
 #Function for data transformation 
-require(DESeq2)
-Transform <-function (data_es,method="log"){
-  if(missing(method)){
-    data_es_log<-log10(data_es)
-    return(data_es_log)
-  }
-  else if(method=="vst"){
-    data_es_vst<-vst(data_es)
-    return(data_es_vst)
-  }
-   else if(method=="sqrt"){
-     data_es_sqrt<-sqrt(data_es)
-     return(data_es_sqrt)
-   }
-  else{
-    data_es_log<-log10(data_es)
-    return(data_es_log)
-  }
+require(edgeR)
+Transform <-function (data_es){
+  
+    data_es_log_cpm<-cpm(data_es, log=TRUE)
+    return(data_es_log_cpm)
+  
 }
 
 
