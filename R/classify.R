73bb7718c4eeb5ea24e83e98df2e965750085f52
a-thind <26285885+a-thind@users.noreply.github.com>
2022-03-15 22:54:58 +0000

    Update description and classify function docs

--- R/classify.R
+++ R/classify.R
@@ -1,24 +1,19 @@
-# Classifier function for classifying samples according to pathway activity
-# @author AT
-
-# load libraries
-require(GSVA)
-
-#' classifies samples according to pathway activity using GSVA
-#'
+#' Classifies samples according to pathway activity using GSVA
+#' @author Anisha Thind \email{a.thind@@cranfield.ac.uk}
 #' @param sigDf Signature dataframe
 #' @param dataSe Expression dataset matrix
-#' @param thresh Threshold between 0-1 for pathway classification (default: 0.05)
+#' @param thresh Threshold between 0-1 for pathway classification (default: 0.25)
 #'
 #' @return classesDf
 #' @export
 #'
 #' @examples
-#' Default threshold
+#' # Default threshold
 #' classify(HER2_sig_df, HER_data_se)
-#' Threshold = 0.25 (top 25% of up-regulated and bottom 25% of down-regulated genes)
-#' classify(HER2_sig_df, HER_data_se, thresh=0.25)
-classify <- function(sigDf, dataSe, thresh=0.05) {
+#' # Threshold = 0.05 (top 5% of up-regulated and bottom 5% of down-regulated genes)
+#' classify(HER2_sig_df, HER_data_se, thresh=0.05)
+classify <- function(sigDf, dataSe, thresh=0.25) {
+  require(GSVA)
   # create list of gene sets (up-regulated and then down regulated) for gsva
   sigs <- list()
   sigs$up <- sigDf[sigDf$expression == 1, 1]
@@ -42,7 +37,6 @@ classify <- function(sigDf, dataSe, thresh=0.05) {
   scoresDn <- as.data.frame(scores[,2])
   colnames(scoresDn) <- "dn"
 
-
   # sort GSVA scores in descending order for the up-regulated
   # and down-regulated set
   sortedUp <- scoresUp[order(scoresUp$up, decreasing=T), ,drop=F]
@@ -61,7 +55,6 @@ classify <- function(sigDf, dataSe, thresh=0.05) {
   classesDf <- data.frame(sample=rownames(sortedUp),
                           class=vector(mode="character", nrow(sortedUp)))
   row.names(classesDf) <- classesDf$samples
-
   # loop through genes if a gene has high score for up-regulated and has
   # low score for down-regulated genes, then that sample is positive for pathway
   # if reverse then negative, else uncertain
@@ -81,7 +74,6 @@ classify <- function(sigDf, dataSe, thresh=0.05) {
 
   # add vector to pathway df
   classesDf$class <- classes
-
   cat("Summary of sample classification based on pathway activity:\n")
   cat("--------------------------------------------------------------\n")
   cat(sprintf("Total number of samples: %d\n", nrow(classesDf)))
