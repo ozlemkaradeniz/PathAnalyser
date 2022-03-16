ef0249a335f4c1f2902ab887a02441429d0c5fe1
a-thind <26285885+a-thind@users.noreply.github.com>
2022-03-16 11:36:47 +0000

    Fix current threshold and matrix/df checking

--- R/classify.R
+++ R/classify.R
@@ -1,10 +1,10 @@
 #' Classifies samples according to pathway activity using GSVA
 #' @author Anisha Thind \email{a.thind@@cranfield.ac.uk}
-#' @param sigDf Signature dataframe
-#' @param dataSe Expression dataset matrix
+#' @param sig_df Signature dataframe
+#' @param data_se Expression dataset matrix
 #' @param thresh Threshold between 0-1 for pathway classification (default: 0.25)
 #'
-#' @return classesDf
+#' @return classes_df
 #' @export
 #'
 #' @examples
@@ -12,71 +12,85 @@
 #' classify(HER2_sig_df, HER_data_se)
 #' # Threshold = 0.05 (top 5% of up-regulated and bottom 5% of down-regulated genes)
 #' classify(HER2_sig_df, HER_data_se, thresh=0.05)
-classify <- function(sigDf, dataSe, thresh=0.25) {
+classify <- function(sig_df, data_se, thresh=0.25) {
   require(GSVA)
+  # check signature arg is dataframe
+  if (!is.data.frame(sig_df)) {
+    stop("Signature argument is not a dataframe.")
+  }
+
+  # check input is input matrix
+  if (is.data.frame(data_se)) {
+    data_se <- as.matrix(data_se)
+  } else if (!is.matrix(data_se)) {
+    stop("Expression dataset is not a matrix or dataframe object.")
+  }
+
   # create list of gene sets (up-regulated and then down regulated) for gsva
   sigs <- list()
-  sigs$up <- sigDf[sigDf$expression == 1, 1]
-  sigs$dn <- sigDf[sigDf$expression == -1, 1]
+  sigs$up <- sig_df[sig_df$expression == 1, 1]
+  sigs$dn <- sig_df[sig_df$expression == -1, 1]
 
   # run GSVA on expression data using 2 gene sets (up-regulated and
   # down-regulated) of the signature
-  if (typeof(dataSe) == "integer") {
+  if (typeof(data_se) == "integer") {
     # If the data is of type integer, these are raw counts and follow Poisson
-    scores <- gsva(dataSe, sigs, kcdf="Poisson", verbose=F)
+    scores <- gsva(data_se, sigs, kcdf="Poisson", verbose=F)
   } else {
-    scores <- gsva(dataSe, sigs, verbose=F)
+    scores <- gsva(data_se, sigs, verbose=F)
   }
 
   # transpose the matrix
   scores <- t(scores)
   # create an up and down regulated matrix
-  scoresUp <- as.data.frame(scores[,1], row.names=rownames(scores))
-  colnames(scoresUp) <- "up"
+  scores_up <- as.data.frame(scores[,1], row.names=rownames(scores))
+  colnames(scores_up) <- "up"
 
-  scoresDn <- as.data.frame(scores[,2])
-  colnames(scoresDn) <- "dn"
+  scores_dn <- as.data.frame(scores[,2])
+  colnames(scores_dn) <- "dn"
 
   # sort GSVA scores in descending order for the up-regulated
   # and down-regulated set
-  sortedUp <- scoresUp[order(scoresUp$up, decreasing=T), ,drop=F]
-  sortedDn <- scoresDn[order(scoresDn$dn, decreasing=T), ,drop=F]
+  sorted_up <- scores_up[order(scores_up$up, decreasing=T), ,drop=F]
+  sorted_dn <- scores_dn[order(scores_dn$dn, decreasing=T), ,drop=F]
 
   # compute quantiles according to user-specified threshold
   if (thresh > 0 && thresh < 1) {
-    threshUp <- quantile(sortedUp$up, probs=seq(0, 1, thresh))
-    threshDn <- quantile(sortedDn$dn, probs=seq(0, 1, thresh))
+    thresh_up <- quantile(sorted_up$up, probs=seq(0, 1, thresh))
+    thresh_dn <- quantile(sorted_dn$dn, probs=seq(0, 1, thresh))
   } else {
     stop("Threshold given is not a decimal between 0 and 1.")
   }
 
 
   # HER 2 (pathway)
-  classesDf <- data.frame(sample=rownames(sortedUp),
-                          class=vector(mode="character", nrow(sortedUp)))
-  row.names(classesDf) <- classesDf$samples
+  classes_df <- data.frame(sample=rownames(sorted_up),
+                          class=vector(mode="character", nrow(sorted_up)))
+  row.names(classes_df) <- classes_df$samples
   # loop through genes if a gene has high score for up-regulated and has
   # low score for down-regulated genes, then that sample is positive for pathway
   # if reverse then negative, else uncertain
-  classesList <- lapply(rownames(sortedUp), function(i) {
-    if (sortedUp[i, 1] > threshUp[4] && sortedDn[i, 1] < threshDn[2]) {
-      classesDf[i, 2] <- "Positive"
-    } else if (sortedUp[i, 1] < threshUp[2] && sortedDn[i, 1] > threshDn[4]){
-      classesDf[i, 2] <- "Negative"
+  classes_list <- lapply(rownames(sorted_up), function(i) {
+   if (sorted_up[i, 1] > thresh_up[length(thresh_up) - 1] &&
+       sorted_dn[i, 1] < thresh_dn[2]) {
+      classes_df[i, 2] <- "Positive"
+    } else if (sorted_up[i, 1] < thresh_up[2] &&
+               sorted_dn[i, 1] > thresh_dn[length(thresh_dn) - 1]){
+      classes_df[i, 2] <- "Negative"
     } else {
-      classesDf[i, 2] <- "Uncertain"
+     classes_df[i, 2] <- "Uncertain"
     }
   })
 
   # convert list to vector
-  classes <- unlist(classesList)
+  classes <- unlist(classes_list)
   classes <- factor(classes, levels=c("Positive", "Negative", "Uncertain"))
 
   # add vector to pathway df
-  classesDf$class <- classes
+  classes_df$class <- classes
   cat("Summary of sample classification based on pathway activity:\n")
   cat("--------------------------------------------------------------\n")
-  cat(sprintf("Total number of samples: %d\n", nrow(classesDf)))
-  print(summary(classesDf$class))
-  return(classesDf)
+  cat(sprintf("Total number of samples: %d\n", nrow(classes_df)))
+  print(summary(classes_df$class))
+  return(classes_df)
 }
