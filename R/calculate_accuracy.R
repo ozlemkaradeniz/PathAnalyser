df031671ccb9d8a63d035a7b5ab2f20d1e0d962f
ozlemkaradeniz <ozlemkaradeniz@gmail.com>
2022-03-19 22:10:59 +0000

    calculation of accuracy v2

--- R/calculate_accuracy.R
+++ R/calculate_accuracy.R
@@ -1,112 +1,100 @@
-# Clear workspace
-rm(list=ls()) 
+#' Classifies samples according to pathway activity using GSVA
+#' Creates confusion matrix and calculate accuracy of classification method
+#' @author Ozlem Karadeniz \email{ozlem.karadeniz.283@cranfield.ac.uk}
+#' @param true_labels_df actual labels dataframe or matrix
+#' @param predicted_labels_df predicted labels dataframe or matrix generated in classification method
+#'
+#' @return confusion_matrix_HER2
+#' @export
+#'
+#' @examples
+#' calculate_accuracy(HER2_labelDF,Her2_pathway)
 
-install.packages("BiocManager")
-BiocManager::install("GSVA")
-
-# Installing
-install.packages("readr")
-# Loading
-library("readr")
-
-library(GSVA)
-
-setwd("~/GroupProject1")
-
-
-X <- read.table("TCGA_unannotated.txt", header=TRUE,sep = "\t")
-X <- data.matrix(X)
-
-up <- read_lines("ERBB2_UP.V1_UP.grp", skip = 3, n_max = -1L)
-dn <- read_lines("ERBB2_UP.V1_DN.grp", skip = 3, n_max = -1L)
-
-gs_list <- list("up"=up, "dn" = dn)
-
-gsva.es <- gsva(X, gs_list, verbose=FALSE)
-write.csv(gsva.es, "/Users/ozlemkaradeniz/GroupProject1/gsva_scores.csv")
-
-gsva.es.t <- as.data.frame(t(gsva.es))
-
-df_up <- data.frame(samples= rownames(gsva.es.t), up=gsva.es.t[,1])
-df_up_sorted <- df_up[order(df_up$up, decreasing = TRUE),]
-
-df_down <- data.frame(samples= rownames(gsva.es.t), down=gsva.es.t[,2])
-df_down_sorted <- df_down[order(df_down$down, decreasing = TRUE),]
-
-# compute quartiles
-quantUp <- quantile(df_up_sorted$up)
-quantDown <- quantile(df_down_sorted$down)
-
-
-# pathway analyse
-pathway <- data.frame(samples=df_up_sorted[,1], HER2=vector(mode="character", nrow(df_up_sorted)))
-
-pathwayActivity <- lapply(pathway$samples, function(sample) {
-        if (df_up_sorted[df_up_sorted$samples==sample, 2] > quantUp[length(quantUp)-1] && df_down_sorted[df_down_sorted$samples==sample, 2] < quantDown[2]) {
-                "+"
-        } else if (df_up_sorted[df_up_sorted$samples==sample, 2] < quantUp[2] && df_down_sorted[df_down_sorted$samples==sample, 2] > quantDown[length(quantUp)-1]){
-                "-"
-        } else {
-                "?"
+calculate_accuracy <-function(true_labels_df, predicted_labels_df){
+        
+        # check true_classes_df arg is dataframe or matrix
+        if (is.matrix(true_labels_df)){
+                true_labels_df <- as.data.frame(true_labels_df)   
         }
-})
-
-
-pathwayActivity <- factor(pathwayActivity, levels=c("+", "-", "?"))
-
-pathway$HER2 <- pathwayActivity
-
-summary(pathway$HER2)
-pathway
-
-for (i in 1:nrow(pathway)) {
-        pathway[i,1] <- gsub("\\.", "-", pathway[i,1])
-}
-
-labelDF <- read.table("Sample_lable.txt", header=TRUE,sep = "\t")
-
-read_labels=function(labelDataFile){
-        labelDF <- read.table(labelDataFile, header=TRUE,sep = "\t") 
-        return(labelDF)
-}
-
-labelDF <- read_labels("Sample_lable.txt")
-
-compare_classifications = function(true_classes_df, predicted_classes_df){
+        else if (!is.data.frame(true_labels_df)){
+                stop("true_labels_df argument is not a dataframe or matrix.")  
+        }
+                
+        
+        # check predicted_labels_df arg is dataframe or matrix
+        if (is.matrix(predicted_labels_df)){
+                predicted_labels_df <- as.data.frame(predicted_labels_df) 
+        }
+                
+        else if (!is.data.frame(predicted_labels_df)){
+                stop("predicted_labels_df argument is not a dataframe or matrix.")
+        }
+        
+        # HER2 status Equivocal, [Not Evaluated] and Indeterminate are all set to uncertain
+        
+        true_labels_df$HER2 <- sapply(true_labels_df$HER2, 
+                 function(x){gsub(pattern = "(Equivocal|\\[Not Evaluated\\]|Indeterminate)",
+                                  replacement = "Uncertain", x)})
+        
+        for (i in 1:nrow(predicted_labels_df)) {
+                predicted_labels_df[i,1] <- gsub("\\.", "-", predicted_labels_df[i,1])
+        }
+        
         # create confusion matrix
         confusion_matrix_HER2<-matrix(0,nrow = 3,ncol = 3,
                                       dimnames = list(c("Actual_Pos","Actual_Neg","Actual_Uncertain"),
                                                       c("Prediction_Pos","Prediction_Neg", "Prediction_Uncertain")))
-        df<-merge(x=true_classes_df, y=predicted_classes_df, by.x = "CaseID", by.y="samples")
+        df<-merge(x=true_labels_df, y=predicted_labels_df, by.x = "CaseID", by.y="sample")
         
-        true_positive <- nrow(df[(df$HER2.y == '+' & df$HER2.x == "Positive"),]) # predicted positive, actual positive
-        false_positive <- nrow(df[(df$HER2.y == '+' & df$HER2.x == "Negative"),]) # predicted positive, actual negative
+        true_positive <- nrow(df[(df$class == "Positive" & df$HER2 == "Positive"),]) # predicted positive, actual positive
+        false_positive <- nrow(df[(df$class == "Positive" & df$HER2 == "Negative"),]) # predicted positive, actual negative
         confusion_matrix_HER2[1,1] <- true_positive
         confusion_matrix_HER2[2,1] <- false_positive
-        confusion_matrix_HER2[3,1] <- nrow(df[df$HER2.y == '+',])-true_positive-false_positive
+        confusion_matrix_HER2[3,1] <- nrow(df[df$class == 'Positive',])-true_positive-false_positive
         
-        true_negative <- nrow(df[(df$HER2.y == '-' & df$HER2.x == "Negative"),]) # predicted negative, actual negative
-        false_negative <- nrow(df[(df$HER2.y == '-' & df$HER2.x == "Positive"),]) # predicted negative, actual positive
+
+        true_negative <- nrow(df[(df$class == "Negative" & df$HER2 == "Negative"),]) # predicted negative, actual negative
+        false_negative <- nrow(df[(df$class == "Negative" & df$HER2 == "Positive"),]) # predicted negative, actual positive
         confusion_matrix_HER2[2,2] <- true_negative
         confusion_matrix_HER2[1,2] <- false_negative
-        confusion_matrix_HER2[3,2] <- nrow(df[df$HER2.y == '-',])-true_negative-false_negative
+        confusion_matrix_HER2[3,2] <- nrow(df[df$class == 'Negative',])-true_negative-false_negative
         
-        true_uncertain <- nrow(df[(df$HER2.y == '?' & df$HER2.x != "Negative" & df$HER2.x != "Positive"),]) # predicted uncertain, actual uncertain
+        true_uncertain <- nrow(df[(df$class == "Uncertain" & df$HER2 != "Uncertain"),]) # predicted uncertain, actual uncertain
         confusion_matrix_HER2[3,3] <- true_uncertain
-        confusion_matrix_HER2[1,3] <- nrow(df[(df$HER2.y == '?' & df$HER2.x == "Positive"),])  # predicted uncertain, actual positive
-        confusion_matrix_HER2[2,3] <- nrow(df[(df$HER2.y == '?' & df$HER2.x == "Negative"),])  # predicted uncertain, actual negative
-        
+        confusion_matrix_HER2[1,3] <- nrow(df[(df$class == 'Uncertain' & df$HER2 == "Positive"),])  # predicted uncertain, actual positive
+        false_uncertain <- confusion_matrix_HER2[1,3]
+        confusion_matrix_HER2[2,3] <- nrow(df[(df$class == 'Uncertain' & df$HER2 == "Negative"),])  # predicted uncertain, actual negative
+        false_uncertain <- false_uncertain + confusion_matrix_HER2[2,3]
         accuracy <- (true_positive + true_negative + true_uncertain) / nrow(df) * 100
+        
+        print("Confusion Matrix ")
+        print("--------------------------------------------------------------")
+        print(confusion_matrix_HER2)
+
+        print("Accuracy of classification summary ")
+        print("--------------------------------------------------------------")
         print(paste0("accuracy: ", format(round(accuracy, 2), nsmall = 2)))
         print(paste0("total number samples: " , nrow(df)))
-        print(paste0("true_positive: " , true_positive))
-        print(paste0("true_negative: " , true_negative))
-        print(paste0("true_uncertain: " , true_uncertain))
-        print(paste0("sum in confusion_matrix_HER2: " , sum(confusion_matrix_HER2)))
-        print(confusion_matrix_HER2)
+        print(paste0("True Positive(TP): " , true_positive))
+        print(paste0("True Negative(TN): " , true_negative))
+        print(paste0("True Uncertain: " , true_uncertain))
+        print(paste0("False Negative(FN): " , false_negative))
+        print(paste0("False Positive(FP): " , false_positive))
+        print(paste0("False Uncertain: " , false_uncertain))
+        print("--------------------------------------------------------------")
+        print(paste0("True Positive Rate(TPR): ", 
+                     format(round(true_positive / (true_positive + false_negative) * 100 , 2) , nsmall =2)))
+        print(paste0("True Negative Rate(TNR): ", 
+             format(round(true_negative / (true_negative + false_positive) * 100 , 2) , nsmall =2)))
+        print(paste0("False Positive Rate(FPR): ", 
+             format(round(false_positive / (false_positive + true_negative) * 100 ,  2) , nsmall =2)))
+        print(paste0("False Negative Rate(FNR): ", 
+             format(round(false_negative / (false_negative + true_positive) * 100 ,  2) , nsmall =2)))
+        print("--------------------------------------------------------------")
+        print(summary(confusion_matrix_HER2))
+        
+        return(confusion_matrix_HER2)
 } 
-
-compare_classifications(labelDF,pathway)
 
 
 
