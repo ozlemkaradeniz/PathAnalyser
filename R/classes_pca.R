fd37ff8ff0cbec74d2a11be0f6dadc05dc2cf5fa
Yi-Hsuan Lee <n0933330979@gmail.com>
2022-03-21 22:22:08 +0000

    Visualisation PCA

---
+++ R/classes_pca.R
@@ -0,0 +1,19 @@
+
+
+classes_pca <-
+  function(normalized_data_filtered,
+           predicted_labels_df,
+           pathway_name = "ER") {
+    
+    library(ggfortify)
+    
+    data_filtered<-t(normalized_data_filtered)
+
+    Merged<-merge(predicted_labels_df,data_filtered,by='row.names',all=T)
+    Merged<-Merged[,-2]
+    pca_res <- prcomp(Merged[3:ncol(Merged)], scale. = TRUE)
+    autoplot(pca_res, data = Merged, colour = pathway_name)
+   
+  }
+
+
