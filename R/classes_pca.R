a6a50a5460f221302fb28a5478d1145b02210270
Yi-Hsuan <100585761+Yihsuanleecranfiled@users.noreply.github.com>
2022-03-23 14:24:40 +0000

    Update classes_pca.R

--- R/classes_pca.R
+++ R/classes_pca.R
@@ -15,14 +15,14 @@
 classes_pca <-
   function(normalized_data_filtered,
            predicted_labels_df,
-           pathway_name = "ER") {
+           pathway_name = "classes") {
 
     library(ggfortify)
 
     data_filtered<-t(normalized_data_filtered)
 
     Merged<-merge(predicted_labels_df,data_filtered,by='row.names',all=T)
-    Merged<-Merged[,-2]
+    #Merged<-Merged[,-2]
     pca_res <- prcomp(Merged[3:ncol(Merged)], scale. = TRUE)
     autoplot(pca_res, data = Merged, colour = pathway_name)
 
