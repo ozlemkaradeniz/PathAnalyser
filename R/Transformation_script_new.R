003b6e33f8628af62d706c1298f4c2e2eb1f12f5
a-thind <26285885+a-thind@users.noreply.github.com>
2022-03-23 10:19:48 +0000

    Add renamed function change to git message and merge changes

--- R/Transformation_script_new.R
+++ R/Transformation_script_new.R
@@ -1,10 +1,23 @@
-#Function for data transformation 
-require(edgeR)
-Transform <-function (data_es){
-  
+#' Transforms gene expression dataset according to CPM read count normalization
+#' @description Transforms gene expression dataset according to CPM read count normalization,
+#'              calls cmp method in edgeR library
+#'
+#' @author Rishabh Kaushik \email{rishabh.kaushik.126@cranfield.ac.uk}
+#' @param data_es  Gene expression data matrix
+#'
+#'
+#' @return Normalized Gene expression data matrix
+#' @export
+#'
+#' @examples transform(data_es)
+#'
+
+transform <-function (data_es){
+   require(edgeR)
+
     data_es_log_cpm<-cpm(data_es, log=TRUE)
     return(data_es_log_cpm)
-  
+
 }
 
 
