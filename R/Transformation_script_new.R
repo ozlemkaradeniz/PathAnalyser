7a1217c4b03bb27bfc3dda97bbc5de5e4b363c97
Yi-Hsuan Lee <n0933330979@gmail.com>
2022-03-31 21:15:00 +0100

    update

--- R/Transformation_script_new.R
+++ R/Transformation_script_new.R
@@ -1,18 +1,19 @@
-#' Transforms gene expression dataset according to CPM read count normalization
-#' @description Transforms gene expression dataset according to CPM read count normalization,
-#'              calls cmp method in edgeR library
+
+#' Normalisation of the raw count data by using  log CPM (counts per million)
+#' @description  Normalisation of the raw count data by using  log CPM (counts per million) by calling
+#'  cmp method in edgeR library
 #'
 #' @author Rishabh Kaushik \email{rishabh.kaushik.126@cranfield.ac.uk}
-#' @param data_es  Gene expression data matrix
+#' @param data_es  Gene expression data matrix from TCGA
 #'
 #'
 #' @return Normalized Gene expression data matrix
 #' @export
 #'
-#' @examples transform(data_es)
+#' @examples log_cpm(data_es)
 #'
 
-transform <-function (data_es){
+log_cpm <-function (data_es){
    require(edgeR)
 
     data_es_log_cpm<-cpm(data_es, log=TRUE)
