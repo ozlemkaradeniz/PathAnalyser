52cb5a4c3ebf7a0608b9a58bd87aa9980ee691af
Yi-Hsuan Lee <n0933330979@gmail.com>
2022-04-05 18:38:29 +0100

    update

--- R/classes_pca.R
+++ R/classes_pca.R
@@ -5,7 +5,7 @@
 #' @author Yi-Hsuan Lee \email{yi-hsuan.lee@cranfield.ac.uk}
 #' @param normalized_data_filtered  Normalized Gene expression data matrix
 #' @param predicted_labels_df  dataframe which labels for each sample predicted by classification algorithm
-#' @param pathway_name  pathway name ER or HER2, default is ER
+#' @param pathway_name  pathway name, default is "ER"
 #'
 #'
 #' @return
@@ -18,13 +18,24 @@ classes_pca <-
   function(normalized_data_filtered,
            predicted_labels_df,
            pathway_name = "ER") {
-    require(ggfortify)
+
+    require(plotly)
 
     data_filtered<-t(normalized_data_filtered)
     row.names(predicted_labels_df)<-predicted_labels_df$sample
     Merged<-merge(predicted_labels_df,data_filtered,by='row.names',all=T)
     Merged<-Merged[,-1]
-    pca_res <- prcomp(Merged[3:ncol(Merged)], scale. = TRUE)
-    autoplot(pca_res, data = Merged, colour = "class",main=pathway_name)
 
+    prin_comp <- prcomp(Merged[3:ncol(Merged)], rank. = 2)
+    components <- prin_comp[["x"]]
+    components <- data.frame(components)
+    components <- cbind(components, Merged$class)
+    row.names(components)<-Merged$sample
+    pca<-plot_ly(components,type = "scatter",x = ~PC1, y = ~PC2,text=rownames(components),
+            mode="markers",color = components$`Merged$class`,marker=list(size=11))%>%
+      layout(title=pathway_name,
+             xaxis=list(title="PC1"),
+             yaxis=list(title="PC2"))
+    pca
   }
+
