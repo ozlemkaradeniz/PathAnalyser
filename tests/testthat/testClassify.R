685b1b30cc7061e69be8279bc89d20acb8eddf3b
a-thind <26285885+a-thind@users.noreply.github.com>
2022-04-20 00:18:52 +0100

    Switch parameter order

--- tests/testthat/testClassify.R
+++ tests/testthat/testClassify.R
@@ -5,7 +5,7 @@ data("ER_TCGA_RNAseq")
 load("normalized_ER_TCGA_RNAseq.rda")
 load("ER_classes_df.default_actual.rda")
 
-ER_classes_df.default<- classify_GSVA_percent(ER_sig_df, normalized_ER_TCGA_RNAseq)
+ER_classes_df.default<- classify_GSVA_percent(normalized_ER_TCGA_RNAseq, ER_sig_df)
 
 expect_equal(ER_classes_df.default_actual, ER_classes_df.default)
 
