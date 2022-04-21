957ba9603b65bc57f49bb31cbc8d7007dd2f79aa
a-thind <26285885+a-thind@users.noreply.github.com>
2022-04-21 23:42:41 +0100

    Update classify function name change

--- tests/testthat/testClassify.R
+++ tests/testthat/testClassify.R
@@ -5,7 +5,7 @@ data("ER_TCGA_RNAseq")
 load("normalized_ER_TCGA_RNAseq.rda")
 load("ER_classes_df.default_actual.rda")
 
-ER_classes_df.default<- classify_GSVA_percent(normalized_ER_TCGA_RNAseq, ER_sig_df)
+ER_classes_df.default<- classify_gsva_percent(normalized_ER_TCGA_RNAseq, ER_sig_df)
 
 expect_equal(ER_classes_df.default_actual, ER_classes_df.default)
 
